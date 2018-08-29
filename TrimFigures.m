b8b9acca7821aaf741048b4b55a319fd216b839c
Tom Holden <thomas.holden@gmail.com>
2018-08-29 11:27:08 +0200

    display figure titles

--- TrimFigures.m
+++ TrimFigures.m
@@ -1,5 +1,6 @@
 for i = 1 : 24
     openfig( [ int2str( i ) '.fig' ] );
+    disp( [ int2str( i ) ' ' get( gcf, 'Name' ) ] );
     set( gcf, 'WindowState', 'maximized' );
     drawnow;
     tightfig( gcf );
